9dcf4d44c59aa0596ede3207871550e8feb75347
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2026-05-25 18:27:14 +0530

    Add Square Hollow Pattern

---
+++ src/Coding_Exercises/Square_Hollow_Pattern.java
@@ -0,0 +1,32 @@
+package Coding_Exercises;
+
+import java.util.Scanner;
+
+public class Square_Hollow_Pattern {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = obj.nextInt();
+
+        for (int i=1 ; i<=num ; i++) {
+            for (int j=1 ; j<=num ; j++) {
+                if (i == 1 || i == num || j == 1 || j == num) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// Output
+//Enter a number : 5
+//        * * * * *
+//        *       *
+//        *       *
+//        *       *
+//        * * * * *
