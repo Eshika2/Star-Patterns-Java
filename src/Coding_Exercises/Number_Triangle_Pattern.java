2d2fa7650bd8ab035c6b7d4e332c18ee1974cdf5
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2026-06-02 20:13:08 +0530

    Update Number_Triangle_Pattern

--- src/Coding_Exercises/Number_Triangle_Pattern.java
+++ src/Coding_Exercises/Number_Triangle_Pattern.java
@@ -14,7 +14,10 @@ public class Number_Triangle_Pattern {
             for (int j=num ; j>i ; j--) {
                 System.out.print(" ");
             }
-//            System.out.println();
+
+//            for (int j=1 ; j<=i ; j++) {
+//                System.out.print("*" + " ");
+//            }
 
             for (int j=1 ; j<=i ; j++) {
                 System.out.print(i + " ");
@@ -29,9 +32,10 @@ public class Number_Triangle_Pattern {
 
 // Output
 
-//Enter the number : 5
-//    1
-//   2 2
-//  3 3 3
-// 4 4 4 4
-//5 5 5 5 5
+
+// Enter the number : 5
+//     1
+//    2 2
+//   3 3 3
+//  4 4 4 4
+// 5 5 5 5 5
