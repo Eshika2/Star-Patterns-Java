e29775b5ecfe76e6b0fa981271c6b16345564f2c
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2026-06-10 08:44:46 +0530

    Add H_Rhombus_Pattern

--- src/Coding_Exercises/H_Rhombus_Pattern.java
+++ src/Coding_Exercises/H_Rhombus_Pattern.java
@@ -1,5 +1,37 @@
 package Coding_Exercises;
 
 public class H_Rhombus_Pattern {
+    public static void main(String[] args) {
+        int num = 6;
 
+        for (int i=1 ; i<=num ; i++) {
+            // spaces
+            for (int j=num ; j>i ; j--) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j=1 ; j<=num ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
+
+// Output
+
+//     ******
+//    ******
+//   ******
+//  ******
+// ******
+//******
+
+//_____******
+//____******
+//___******
+//__******
+//_******
+//******
