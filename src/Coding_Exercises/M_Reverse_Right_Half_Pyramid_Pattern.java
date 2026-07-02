66d4d03f94a4059944f34ef7099a83966222eb83
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2026-07-02 09:11:58 +0530

    Add M_Reverse_Right_Half_Pyramid_Pattern

---
+++ src/Coding_Exercises/M_Reverse_Right_Half_Pyramid_Pattern.java
@@ -0,0 +1,35 @@
+package Coding_Exercises;
+
+import java.util.Scanner;
+
+public class M_Reverse_Right_Half_Pyramid_Pattern {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = obj.nextInt();
+
+        for (int i=num ; i>=1 ; i--) {
+            for (int j=1 ; j<=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output
+//******
+//*****
+//****
+//***
+//**
+//*
+
+//Enter the number : 6
+//******
+//*****
+//****
+//***
+//**
+//*
